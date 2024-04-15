91aa51faca546562e4b911924355402b33a0232d
sangshrestha <sang.socials@gmail.com>
2024-04-15 15:02:45 +0100

    feat: style body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} flex justify-center m-10 bg-stone-100`}>{children}</body>
     </html>
   );
 }
