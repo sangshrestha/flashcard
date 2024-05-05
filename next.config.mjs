e96b8e274eef296287e9fa90cbd33ed4bf6880ec
sangshrestha <sang.socials@gmail.com>
2024-05-05 13:02:59 +0100

    fix: dynamic basepath

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { basePath: "/flashcard", output: "export" };
+const nextConfig = {
+  basePath: process.env.NODE_ENV === "development" ? "" : "/flashcard",
+  output: "export",
+};
 
 export default nextConfig;
